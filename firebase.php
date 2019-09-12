fcc0ed5375941104fd83cc319d63106a8fe3830a
Renan Pereira <renansoaresinfo@gmail.com>
2019-09-12 15:14:29 -0300

    Adiciona arquivo firebase.php

---
+++ firebase.php
@@ -0,0 +1,20 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.6.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCALtDsQzWV6PoJ7WHHU6g430cvvBlrBXs",
+    authDomain: "cornout-235f3.firebaseapp.com",
+    databaseURL: "https://cornout-235f3.firebaseio.com",
+    projectId: "cornout-235f3",
+    storageBucket: "",
+    messagingSenderId: "659446088612",
+    appId: "1:659446088612:web:9b797ee2c1857bae8bf731"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
