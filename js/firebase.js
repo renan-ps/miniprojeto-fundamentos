00adede9a2d86c45d629e1bcecb119884d4701b8
Renan Pereira <renansoaresinfo@gmail.com>
2019-09-12 15:27:43 -0300

    Altera e corrige arquivo do firebase

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyCALtDsQzWV6PoJ7WHHU6g430cvvBlrBXs",
+		authDomain: "cornout-235f3.firebaseapp.com",
+		databaseURL: "https://cornout-235f3.firebaseio.com",
+		projectId: "cornout-235f3",
+		storageBucket: "",
+		messagingSenderId: "659446088612",
+		appId: "1:659446088612:web:9b797ee2c1857bae8bf731"
+	};
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
